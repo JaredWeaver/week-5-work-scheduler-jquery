7f8d058e6ca736935ebbbbd72b48e9521f58b12a
JaredWeav <jared.weaver2@gmail.com>
2021-01-04 12:17:42 -0500

    added Readme content

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-## Your Task
+
+
+## Task
 
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use a library like [Moment.js](https://momentjs.com/) to work with dates and times. `Moment.js` has historically been the most popular date/time library but is no longer supported by its developers. However, you can still use it for this project, or you can look into one of the following alternatives:
-
-  * [Luxon](https://moment.github.io/luxon/)
-
-  * [Day.js](https://day.js.org/)
-
-  * [date-fns](https://date-fns.org/)
-
-  * [js-Joda](https://js-joda.github.io/js-joda/)
-
-Whichever library you choose, be sure to read the documentation carefully!
-
 
 ## User Story
 
@@ -45,8 +34,3 @@ THEN the saved events persist
 ```
 
 
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 
 var h = [""];
 var hours;
+var userTasks;
 
 
 //Display day and date in the header
@@ -16,6 +17,12 @@ displayDay()
 
 $(document).ready(function() {
 
+  $('.saveBtn').on("click", function(event) {
+    var hour = $(this).attr('id');
+    var userTasks = $(this).siblings('.description').val();
+    localStorage.setItem(hour, userTasks);
+    
+  });
     // Colors of the blocks change as time progresses
   function colorCodeBlocks() {
     
@@ -23,30 +30,32 @@ $(document).ready(function() {
     var currentHour = moment().hour();    
     // Loop through <textarea> ID's and parse/split them into a string
     $('.description').each(function () {
-        var userTime = parseInt($(this).attr('id').split()[0]);
+        var userTime = parseInt($(this).attr('id').split('h')[1]);
     // 
         if (userTime < currentHour){
-          $(this).removeClass('future');
-          $(this).removeClass('present');
           $(this).addClass('past');
         } else if (userTime === currentHour){
-          $(this).removeClass('future');
-          $(this).removeClass('past');
           $(this).addClass('present');
         } else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
           $(this).addClass('future');
         }
-
     });
   }
+    
+ 
+  $("#h9.description").val(window.localStorage.getItem("n9"));
+  $("#h10.description").val(window.localStorage.getItem("n10"));
+  $("#h11.description").val(window.localStorage.getItem("n11"));
+  $("#h12.description").val(window.localStorage.getItem("n12"));
+  $("#h13.description").val(window.localStorage.getItem("n13"));
+  $("#h14.description").val(window.localStorage.getItem("n14"));
+  $("#h15.description").val(window.localStorage.getItem("n15"));
+  $("#h16.description").val(window.localStorage.getItem("n16"));
+  $("#h17.description").val(window.localStorage.getItem("n17"));
+
+  
+  
+
   colorCodeBlocks()
+  
 });
-
-
-
-
-
-
-
